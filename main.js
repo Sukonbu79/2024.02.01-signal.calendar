4d94796298760bebd110c92d0c7f6d8aa3ce01ab
Sukonbu79 <149184637+Sukonbu79@users.noreply.github.com>
2024-02-01 21:48:38 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+"use strict";
+
+{
+  // 信号機
+  const blue = document.querySelector("#blue");
+  const yellow = document.querySelector("#yellow");
+  const red = document.querySelector("#red");
+  const btn = document.querySelector("#btn");
+
+  btn.addEventListener("click", () => {
+    btn.disabled = true;
+    setTimeout(() => {
+      red.classList.add("gray-color");
+      blue.classList.remove("gray-color");
+      blue.classList.add("blue-color");
+    }, 2000);
+    setTimeout(() => {
+      blue.classList.add("blue-blink");
+    }, 5500);
+    setTimeout(() => {
+      blue.classList.remove("blue-blink");
+      blue.classList.add("gray-color");
+      yellow.classList.remove("gray-color");
+      yellow.classList.add("yellow-color");
+    }, 8500);
+    setTimeout(() => {
+      yellow.classList.remove("yellow-color");
+      yellow.classList.add("gray-color");
+      red.classList.remove("gray-color");
+      red.classList.add("red-color");
+      btn.disabled = false;
+    }, 11000);
+  });
+
+  // カレンダー
+  function setDay() {
+    let d = new Date();
+    let year = d.getFullYear();
+    let month = String(d.getMonth() + 1).padStart(2, 0);
+    let day = String(d.getDate()).padStart(2, 0);
+    let date = `${year}年${month}月${day}日`;
+    document.querySelector("#day").textContent = date;
+  }
+
+  setDay();
+
+  // 曜日
+  function setWeek() {
+    let d = new Date();
+    let week = d.getDay();
+    let weekArray = ["(日)", "(月)", "(火)", "(水)", "(木)", "(金)", "(土)"];
+    document.querySelector("#week").textContent = weekArray[week];
+  }
+
+  setWeek();
+
+  // 時計
+  function setTime() {
+    let d = new Date();
+    let h = String(d.getHours()).padStart(2, 0);
+    let m = String(d.getMinutes()).padStart(2, 0);
+    let s = String(d.getSeconds()).padStart(2, 0);
+    let time = `${h}:${m}:${s}`;
+    document.querySelector("#clock").textContent = time;
+  }
+
+  setTime();
+  setInterval(() => {
+    setTime();
+  }, 100);
+}
